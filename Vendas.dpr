c3146bf36bc6ab53e6b20c3934228a6399e29f1f
crispoletto <cris.poletto@hotmail.com>
2018-03-24 19:08:32 -0300

    Alteração alinhamento label "Bloqueado"

--- Vendas.dpr
+++ Vendas.dpr
@@ -2,7 +2,6 @@ program Vendas;
 
 uses
   Vcl.Forms,
-<<<<<<< HEAD
   UntPadrao1 in 'UntPadrao\UntPadrao1.pas' {FrmPadrao1},
   UntPesqData in 'UntPadrao\UntPesqData.pas' {pesqData},
   UntPesqString in 'UntPadrao\UntPesqString.pas' {pesqString},
@@ -12,18 +11,6 @@ uses
   UntPerfil in 'UntGeral\UntPerfil.pas' {FrmPerfil},
   UntCliente in 'UntCadastro\UntCliente.pas' {FrmCliente},
   UntUsuario in 'UntCadastro\UntUsuario.pas' {FrmUsuario};
-=======
-  UntMenuPrincipal in 'UntMenuPrincipal.pas' {FrmMenuPrincipal},
-  UntDM in 'UntDM.pas' {DM: TDataModule},
-  UntPadrao1 in 'UntPadrao1.pas' {FrmPadrao1},
-  UntUsuario in 'UntUsuario.pas' {FrmUsuario},
-  UntPerfil in 'UntPerfil.pas' {FrmPerfil},
-  UntLogin in 'UntLogin.pas' {FrmLogin},
-  UntPesqData in 'UntPesqData.pas' {pesqData},
-  UntPesqString in 'UntPesqString.pas' {pesqString},
-  UntCliente in 'UntCliente.pas' {FrmCliente};
-  UntMarca in 'UntMarca.pas' {FrmPadrao2};
->>>>>>> f8d3ec052407afe6c80c77d31ad379f6af778474
 
 {$R *.res}
 
@@ -38,10 +25,6 @@ begin
   Application.CreateForm(TFrmLogin, FrmLogin);
   Application.CreateForm(TFrmPerfil, FrmPerfil);
   Application.CreateForm(TFrmCliente, FrmCliente);
-<<<<<<< HEAD
   Application.CreateForm(TFrmUsuario, FrmUsuario);
-=======
-  Application.CreateForm(TFrmPadrao2, FrmPadrao2);
->>>>>>> f8d3ec052407afe6c80c77d31ad379f6af778474
   Application.Run;
 end.
