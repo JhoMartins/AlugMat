5ac6fdacb6fc233c91141275d9dc804baf4f6d83
crispoletto <cris.poletto@hotmail.com>
2018-05-26 00:26:51 -0300

    Finalizar Reservas Automaticamente
    - Reservas feitas há mais de 5 dias corridos são finalizadas automaticamente ao acessar o formulário

--- UntRelatorio/UntRelReservasWeb.pas
+++ UntRelatorio/UntRelReservasWeb.pas
@@ -24,8 +24,16 @@ type
     FDQuery2: TFDQuery;
     frxDBDataset2: TfrxDBDataset;
     frxReport1: TfrxReport;
+    btn_limpar: TBitBtn;
+    FDTabela: TFDTable;
+    FDTabelaID: TFDAutoIncField;
+    FDTabelaCD_CLIENTE: TIntegerField;
+    FDTabelaSTATUS: TStringField;
+    FDTabelaDATA_RESERVA: TDateTimeField;
+    FDCommand1: TFDCommand;
     procedure btn_imprimirClick(Sender: TObject);
     procedure btn_cancelarClick(Sender: TObject);
+    procedure DataSource1DataChange(Sender: TObject; Field: TField);
   private
     { Private declarations }
   public
@@ -47,6 +55,39 @@ end;
 procedure TFrmRelReservaWeb.btn_imprimirClick(Sender: TObject);
 begin
   frxReport1.ShowReport();
+end;
+
+procedure TFrmRelReservaWeb.DataSource1DataChange(Sender: TObject;
+  Field: TField);
+var msg: string;
+    finaliza: boolean;
+begin
+  msg := 'As reservas abaixo foram finalizadas automaticamente:' + #13#13;
+  finaliza := false;
+  FDTabela.Open();
+  FDTabela.First;
+
+  while not FDTabela.Eof do
+  begin
+    if ((Date > (FDTabelaDATA_RESERVA.AsDateTime + 5)) and (FDTabelaSTATUS.AsString = '')) then
+    begin
+      finaliza := true;
+
+      FDCommand1.CommandText.Clear;
+      FDCommand1.CommandText.Add('UPDATE PRODUTO P SET P.DISPONIVEL = ' + #39 + 'S' + #39 + ' WHERE ID IN (SELECT IR.CD_PRODUTO FROM ITENS_RESERVA IR WHERE IR.CD_PRODUTO = P.ID AND IR.CD_RESERVA = ' + FDTabelaID.AsString + ')');
+      FDCommand1.Execute();
+
+      FDCommand1.CommandText.Clear;
+      FDCommand1.CommandText.Add('UPDATE RESERVAS SET STATUS = ' + #39 + 'A' + #39 + ' WHERE ID = ' + FDTabelaID.AsString);
+      FDCommand1.Execute();
+
+      msg := msg + 'Cód: ' + FDTabelaID.AsString + ' | Data Reserva: ' + FDTabelaDATA_RESERVA.AsString + ' | Cód Cliente: ' + FDTabelaCD_CLIENTE.AsString + #13;
+    end;
+    FDTabela.Next
+  end;
+
+  if finaliza = true then
+  ShowMessage(msg);
 end;
 
 end.
